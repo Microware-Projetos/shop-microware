ccd10c7fea2b09c4de6bf7bc548191734ee1503e
Maycon Jardim <maycon.jardim@microware.com.br>
2025-05-28 19:01:19 +0000

    Ajustes Email Pix

---
+++ woocommerce/woocommerce/emails/admin-pix-receipt-sent.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Admin PIX receipt sent email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/admin-pix-receipt-sent.php.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 4.0.0
+ */
+
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
+use Piggly\WooPixGateway\Core\Entities\PixEntity;
+
+if ( !defined('ABSPATH') ) { exit; }
+
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
+
+/** @var PixEntity $pix */
+
+/*
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<?php
+echo $email_improvements_enabled ? '<div class="email-introduction">' : '';
+?>
+<p><?php printf( esc_html__( 'O consumidor enviou o comprovante de pagamento para o pedido #%s.', 'wc-piggly-pix' ), esc_html( $order->get_order_number() ) ); ?></p>
+<p><?php printf(__( '<a href="%s">Clique aqui</a> para visualizar o comprovante', 'wc-piggly-pix' ), $order->get_edit_order_url()); ?></p>
+<?php echo $email_improvements_enabled ? '</div>' : ''; ?>
+
+<?php
+/*
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+ * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+ * @since 2.5.0
+ */
+do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+
+/*
+ * @hooked WC_Emails::order_meta() Shows order meta data.
+ */
+do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+
+/*
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+ * Show user-defined additional content - this is set in each email's settings.
+ */
+if ( $additional_content ) {
+    echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td class="email-additional-content">' : '';
+    echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+    echo $email_improvements_enabled ? '</td></tr></table>' : '';
+}
+
+/*
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+do_action( 'woocommerce_email_footer', $email );
